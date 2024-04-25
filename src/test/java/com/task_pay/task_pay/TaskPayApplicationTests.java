f587193c973a4d4f752229d51a5766e4916e0ddb
GnyanRanjanBehera <gnyanranjanbehera@gmail.com>
2024-04-25 10:51:29 +0530

    intialize 4/25/2024

---
+++ src/test/java/com/task_pay/task_pay/TaskPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_pay.task_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
