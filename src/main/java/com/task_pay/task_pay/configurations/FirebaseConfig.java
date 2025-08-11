6e452b41d29a3237bc5426c8b4a589fdb2a906ff
Gnyan-Coder <gnyanranjanbehera@gmail.com>
2025-08-11 23:17:34 +0530

    added jar file name

--- src/main/java/com/task_pay/task_pay/configurations/FirebaseConfig.java
+++ src/main/java/com/task_pay/task_pay/configurations/FirebaseConfig.java
@@ -1,30 +1,32 @@
 package com.task_pay.task_pay.configurations;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
-
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
 
 @Configuration
 public class FirebaseConfig {
 
     @Value("${app.firebase-configuration-file}")
-    private String firebaseConfigPath;
+    private Resource firebaseConfig;
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+        try (InputStream serviceAccount = firebaseConfig.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        return FirebaseApp.initializeApp(options);
-
+            return FirebaseApp.initializeApp(options);
+        }
     }
 }
 
